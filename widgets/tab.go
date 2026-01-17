6ba9fe21ad16b6fd16f841460b71c92adf80cb36
Udan Jayanith <j.udanjayanith@gmail.com>
2026-01-17 12:35:09 +0530

    Add tab bar build and layout methods

--- widgets/tab.go
+++ widgets/tab.go
@@ -3,12 +3,12 @@ package CWidget
 import (
 	gui "github.com/guigui-gui/guigui"
 	widget "github.com/guigui-gui/guigui/basicwidget"
+	basic "API-Client/basic"
 )
 
 type TabItem struct {
 	gui.DefaultWidget
 	text widget.Text
-	body gui.Widget
 }
 
 type TabBar struct {
@@ -16,13 +16,32 @@ type TabBar struct {
 	tab_items []*TabItem
 }
 
-type tab_body struct {
-	gui.DefaultWidget
+func (tb *TabBar) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
+	for _, item := range tb.tab_items {
+		adder.AddChild(item)
+	}
+
+	return nil
+}
+
+func (tb *TabBar) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBounds, layouter *gui.ChildLayouter) {
+	layout := gui.LinearLayout{
+		Direction: gui.LayoutDirectionHorizontal,
+		Items: make([]gui.LinearLayoutItem, 0, len(tb.tab_items)),
+	}
+	
+	for _, item := range tb.tab_items {
+		layout.Items = append(layout.Items, gui.LinearLayoutItem{
+			Widget: item,
+		})
+	}
+
+	layout = basic.Align(layout, basic.Start, basic.Center)
+	layout.LayoutWidgets(ctx, widgetBounds.Bounds(), layouter)
 }
 
 type TabContainer struct {
 	gui.DefaultWidget
 	tab_bar *TabBar
-	body tab_body
 }
 
