874d1ab94940c722d4692e23ad0a470cb378b5eb
Udan Jayanith <j.udanjayanith@gmail.com>
2026-02-09 01:13:02 +0530

    Changes

--- common-widgets/tab.go
+++ common-widgets/tab.go
@@ -15,7 +15,7 @@ type TabItem[T any] struct {
 	gui.DefaultWidget
 
 	Text                     string
-	Size                    gui.Size
+	Size                     gui.Size
 	Value                    T
 	text_widget              widget.Text
 	is_selected, is_hovering bool
@@ -26,7 +26,7 @@ func (item *TabItem[T]) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
 	item.text_widget.SetTabular(true)
 	item.text_widget.SetVerticalAlign(widget.VerticalAlignMiddle)
 	item.text_widget.SetHorizontalAlign(widget.HorizontalAlignLeft)
-	
+
 	adder.AddChild(&item.text_widget)
 	return nil
 }
@@ -81,7 +81,7 @@ func (tab *tab[T]) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBounds, layo
 	for i := range tab.tab_items {
 		tab_item := &tab.tab_items[i]
 		layout.Items = append(layout.Items, gui.LinearLayoutItem{
-			Size: tab_item.Size,
+			Size:   tab_item.Size,
 			Widget: tab_item,
 		})
 	}
@@ -100,7 +100,7 @@ func (tab *tab[T]) Measure(ctx *gui.Context, constraints gui.Constraints) image.
 type Tab[T any] struct {
 	gui.DefaultWidget
 	panel widget.Panel
-	tab   tab[T]
+	tab   gui.WidgetWithSize[*tab[T]]
 }
 
 func (tab *Tab[T]) OnSelect(fn func(text string, value T)) {
@@ -112,23 +112,32 @@ func (tab *Tab[T]) GetSelectedIndex() int {
 }
 
 func (tab *Tab[T]) GetTab(index int) (text string, value T) {
-	tab_item := &tab.tab.tab_items[index]
+	tab_item := &tab.tab.Widget().tab_items[index]
 	return tab_item.Text, tab_item.Value
 }
 
 func (tab *Tab[T]) SetTabItems(tab_items []TabItem[T]) {
-	tab.tab.tab_items = tab_items
+	tab.tab.Widget().tab_items = tab_items
 }
 
 func (tab *Tab[T]) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
 	tab.panel.SetContent(&tab.tab)
-	tab.panel.SetContentConstraints(widget.PanelContentConstraintsFixedWidth)
 	tab.panel.SetStyle(widget.PanelStyleSide)
-	tab.panel.SetScrollOffsetByDelta(100, 20)
+	tab.panel.SetContentConstraints(widget.PanelContentConstraintsFixedHeight)
 	adder.AddChild(&tab.panel)
 	return nil
 }
 
 func (tab *Tab[T]) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBounds, layouter *gui.ChildLayouter) {
-	layouter.LayoutWidget(&tab.panel, widgetBounds.Bounds())
+	tab.tab.SetFixedWidth(widgetBounds.Bounds().Dx())
+	layout := gui.LinearLayout{
+		Direction: gui.LayoutDirectionVertical,
+		Items: []gui.LinearLayoutItem{
+			{
+				Widget: &tab.panel,
+				Size:   gui.FixedSize(widget.UnitSize(ctx)),
+			},
+		},
+	}
+	layout.LayoutWidgets(ctx, widgetBounds.Bounds(), layouter)
 }
