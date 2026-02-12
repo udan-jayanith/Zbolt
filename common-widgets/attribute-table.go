11f0cde5c40531ad12ab009e06868acd223eec8a
Udan Jayanith <j.udanjayanith@gmail.com>
2026-02-13 01:31:18 +0530

    Add scratch for attribute table

---
+++ common-widgets/attribute-table.go
@@ -0,0 +1,121 @@
+package CommonWidgets
+
+import (
+	"image"
+
+	gui "github.com/guigui-gui/guigui"
+	widget "github.com/guigui-gui/guigui/basicwidget"
+)
+
+type Attribute struct {
+	gui.DefaultWidget
+
+	Key, Value               string
+	key_widget, value_widget widget.Text
+	delete_widget            widget.Image
+}
+
+func (attr *Attribute) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
+	attr.key_widget.SetTabular(true)
+	attr.key_widget.SetEditable(true)
+	attr.key_widget.SetVerticalAlign(widget.VerticalAlignMiddle)
+	attr.key_widget.SetHorizontalAlign(widget.HorizontalAlignLeft)
+	attr.key_widget.SetValue(attr.Key)
+	attr.key_widget.SetSelectable(true)
+	adder.AddChild(&attr.key_widget)
+
+	attr.value_widget.SetTabular(true)
+	attr.value_widget.SetEditable(true)
+	attr.value_widget.SetVerticalAlign(widget.VerticalAlignMiddle)
+	attr.value_widget.SetHorizontalAlign(widget.HorizontalAlignLeft)
+	attr.value_widget.SetSelectable(true)
+	attr.value_widget.SetValue(attr.Value)
+	adder.AddChild(&attr.value_widget)
+	return nil
+}
+
+func (attr *Attribute) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBounds, layouter *gui.ChildLayouter) {
+	layout := gui.LinearLayout{
+		Direction: gui.LayoutDirectionHorizontal,
+		Items: []gui.LinearLayoutItem{
+			{
+				Widget: &attr.key_widget,
+				Size:   gui.FlexibleSize(1),
+			},
+			{
+				Widget: &attr.value_widget,
+				Size:   gui.FlexibleSize(1),
+			},
+		},
+	}
+	layout.LayoutWidgets(ctx, widgetBounds.Bounds(), layouter)
+}
+
+type attribute_table struct {
+	gui.DefaultWidget
+	rows []Attribute
+}
+
+func (at *attribute_table) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
+	i := len(at.rows) - 1
+	if i == -1 || at.rows[i].Key == "" && at.rows[i].Value == "" {
+		at.rows = append(at.rows, Attribute{})
+	}
+
+	for i := range at.rows {
+		attr_widget := &at.rows[i]
+		adder.AddChild(attr_widget)
+	}
+	return nil
+}
+
+func (at *attribute_table) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBounds, layouter *gui.ChildLayouter) {
+	layout := gui.LinearLayout{
+		Direction: gui.LayoutDirectionVertical,
+		Items:     make([]gui.LinearLayoutItem, 0, len(at.rows)),
+	}
+
+	u := widget.UnitSize(ctx)
+	row_height := u + u/4
+
+	for i := range at.rows {
+		attr_widget := &at.rows[i]
+		layout.Items = append(layout.Items, gui.LinearLayoutItem{
+			Widget: attr_widget,
+			Size:   gui.FixedSize(row_height),
+		})
+	}
+
+	layout.LayoutWidgets(ctx, widgetBounds.Bounds(), layouter)
+}
+
+func (at *attribute_table) Measure(ctx *gui.Context, constraints gui.Constraints) image.Point {
+	var point image.Point
+
+	for i := range at.rows {
+		attr_widget := &at.rows[i]
+		measurements := attr_widget.Measure(ctx, constraints)
+		point.Y += measurements.Y
+		point.X = measurements.X
+	}
+	return point
+}
+
+type AttributeTable struct {
+	gui.DefaultWidget
+
+	attribute_table attribute_table
+	panel           widget.Panel
+}
+
+func (at *AttributeTable) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
+	at.panel.SetContentConstraints(widget.PanelContentConstraintsFixedWidth)
+	at.panel.SetContent(&at.attribute_table)
+	at.panel.SetStyle(widget.PanelStyleSide)
+	adder.AddChild(&at.panel)
+	return nil
+}
+
+func (at *AttributeTable) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBounds, layouter *gui.ChildLayouter) {
+	layouter.LayoutWidget(&at.panel, widgetBounds.Bounds())
+}
