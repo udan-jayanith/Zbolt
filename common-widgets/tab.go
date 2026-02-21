0bbb834b6646e640c4b09c581ea115da2f1000b0
Udan Jayanith <j.udanjayanith@gmail.com>
2026-02-21 17:51:00 +0530

    code changes to tab widget. Mouse events doesn't work for now on tab_items

--- common-widgets/tab.go
+++ common-widgets/tab.go
@@ -9,8 +9,14 @@ import (
 	gui "github.com/guigui-gui/guigui"
 	widget "github.com/guigui-gui/guigui/basicwidget"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
+
+type TabItem[T any] struct {
+	Text     string
+	Value    T
+	Closable bool
+	Icon     *icons.Icon
+}
 
 type tab_item[T any] struct {
 	gui.DefaultWidget
@@ -20,8 +26,10 @@ type tab_item[T any] struct {
 		normal   *icons.Icon
 		selected *icons.Icon
 	}
+	border_widget WidgetWithBorder[*tab_item[T]]
 
-	tab_item *TabItem[T]
+	tab_item   *TabItem[T]
+	tab_widget *tab[T]
 }
 
 func (item *tab_item[T]) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
@@ -33,9 +41,9 @@ func (item *tab_item[T]) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
 	text_widget.SetValue(item.tab_item.Text)
 	text_widget.SetTabular(true)
 	text_widget.SetVerticalAlign(widget.VerticalAlignMiddle)
-	text_widget.SetBold(item.tab_item.is_hovering)
+	//text_widget.SetBold(item.tab_item)
 
-	if item.tab_item.tab.selected_index == item.tab_item.index {
+	if true {
 		text_widget.SetOpacity(1)
 	} else {
 		text_widget.SetOpacity(0.6)
@@ -50,7 +58,7 @@ func (item *tab_item[T]) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
 			item.close_widget.selected = icons.NewIcon("close-grey", size)
 		}
 
-		if item.tab_item.tab.selected_index == item.tab_item.index {
+		if true {
 			item.close_widget.icon = item.close_widget.normal
 		} else {
 			item.close_widget.icon = item.close_widget.selected
@@ -61,7 +69,9 @@ func (item *tab_item[T]) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
 }
 
 func (item *tab_item[T]) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBounds, layouter *gui.ChildLayouter) {
+	u := widget.UnitSize(ctx)
 	layout := gui.LinearLayout{
+		Padding:   basic.NewPadding(u/4, u/2),
 		Gap:       widget.UnitSize(ctx) / 4,
 		Direction: gui.LayoutDirectionHorizontal,
 		Items:     make([]gui.LinearLayoutItem, 0, 3),
@@ -89,6 +99,8 @@ func (item *tab_item[T]) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBounds
 
 func (item *tab_item[T]) Measure(ctx *gui.Context, constraints gui.Constraints) image.Point {
 	point := item.text_widget.Measure(ctx, constraints)
+	u := widget.UnitSize(ctx)
+	padding := basic.NewPadding(u/4, u/2)
 
 	gap := widget.UnitSize(ctx) / 4
 	if item.tab_item.Icon != nil {
@@ -100,82 +112,26 @@ func (item *tab_item[T]) Measure(ctx *gui.Context, constraints gui.Constraints) 
 		icon_measurement := item.close_widget.icon.Measure(ctx, constraints)
 		point.X += icon_measurement.X + gap
 	}
+	point.X += padding.End + padding.Start
+	point.Y = u + u/4
 
 	return point
 }
 
-type TabItem[T any] struct {
-	gui.DefaultWidget
-
-	Text     string
-	Size     gui.Size
-	Value    T
-	Closable bool
-	Icon     *icons.Icon
-
-	border_widget WidgetWithBorder[*gui.WidgetWithPadding[*tab_item[T]]]
-
-	index       int
-	tab         *tab[T]
-	is_hovering bool
-}
-
-func (item *TabItem[T]) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
-	u := widget.UnitSize(ctx)
-
-	padding := gui.WidgetWithPadding[*tab_item[T]]{}
-	padding.SetPadding(basic.NewPadding(u/4, u/2))
-
-	padding.Widget().tab_item = item
-
-	item.border_widget.SetWidget(&padding)
-	adder.AddChild(&item.border_widget)
-	return nil
-}
-
-func (item *TabItem[T]) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBounds, layouter *gui.ChildLayouter) {
-	layout := gui.LinearLayout{
-		Direction: gui.LayoutDirectionHorizontal,
-		Items: []gui.LinearLayoutItem{
-			{
-				Widget: &item.border_widget,
-				Size:   item.Size,
-			},
-		},
-	}
-	layout.LayoutWidgets(ctx, widgetBounds.Bounds(), layouter)
-}
-
-func (item *TabItem[T]) Measure(ctx *gui.Context, constraints gui.Constraints) image.Point {
-	return item.border_widget.Measure(ctx, constraints)
-}
-
-func (tab_item *TabItem[T]) HandlePointingInput(ctx *gui.Context, widgetBounds *gui.WidgetBounds) gui.HandleInputResult {
-	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButton0) && widgetBounds.IsHitAtCursor() {
-		tab_item.tab.selected_index = tab_item.index
-		if tab_item.tab.on_select_fn != nil {
-			tab_item.tab.on_select_fn(tab_item, tab_item.index)
-		}
-	}
-	tab_item.is_hovering = widgetBounds.IsHitAtCursor()
-
-	return gui.HandleInputResult{}
+func (item *tab_item[T]) Draw(ctx *gui.Context, widgetBounds *gui.WidgetBounds, dst *ebiten.Image) {
+	item.border_widget.Draw(ctx, widgetBounds, dst)
 }
 
 type tab[T any] struct {
 	gui.DefaultWidget
-	tab_items      []TabItem[T]
-	selected_index int
-	on_select_fn   func(tab_item *TabItem[T], index int)
+	tab_items []*tab_item[T]
+
+	selected_item_index int
+	on_select_fn        func(tab_item *TabItem[T], index int)
 }
 
 func (tab *tab[T]) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
-	for i := range tab.tab_items {
-		tab_item := &tab.tab_items[i]
-		if tab_item.tab == nil {
-			tab_item.tab = tab
-			tab_item.index = i
-		}
+	for _, tab_item := range tab.tab_items {
 		adder.AddChild(tab_item)
 	}
 	return nil
@@ -187,10 +143,8 @@ func (tab *tab[T]) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBounds, layo
 		Items:     make([]gui.LinearLayoutItem, 0, len(tab.tab_items)),
 	}
 
-	for i := range tab.tab_items {
-		tab_item := &tab.tab_items[i]
+	for _, tab_item := range tab.tab_items {
 		layout.Items = append(layout.Items, gui.LinearLayoutItem{
-			Size:   tab_item.Size,
 			Widget: tab_item,
 		})
 	}
@@ -200,10 +154,9 @@ func (tab *tab[T]) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBounds, layo
 
 func (tab *tab[T]) Measure(ctx *gui.Context, constraints gui.Constraints) image.Point {
 	var point image.Point
-	for i := range tab.tab_items {
-		tab_item := &tab.tab_items[i]
-		mesurement := tab_item.Measure(ctx, constraints)
-		point.X += mesurement.X
+	for _, tab_item := range tab.tab_items {
+		measurement := tab_item.Measure(ctx, constraints)
+		point.X += measurement.X
 	}
 
 	u := widget.UnitSize(ctx)
@@ -219,7 +172,13 @@ type Tab[T any] struct {
 }
 
 func (tab *Tab[T]) SetTabItems(tab_items []TabItem[T]) {
-	tab.tab.tab_items = tab_items
+	tab.tab.tab_items = make([]*tab_item[T], 0, len(tab_items))
+	for _, item := range tab_items {
+		tab.tab.tab_items = append(tab.tab.tab_items, &tab_item[T]{
+			tab_item:   &item,
+			tab_widget: &tab.tab,
+		})
+	}
 }
 
 func (tab *Tab[T]) OnSelect(fn func(tab_item *TabItem[T], index int)) {
@@ -227,21 +186,21 @@ func (tab *Tab[T]) OnSelect(fn func(tab_item *TabItem[T], index int)) {
 }
 
 func (tab *Tab[T]) GetSelectedIndex() int {
-	return tab.tab.selected_index
+	return tab.tab.selected_item_index
 }
 
 func (tab *Tab[T]) GetSelectedTab() (index int, value T) {
-	return tab.tab.selected_index, tab.tab.tab_items[tab.tab.selected_index].Value
+	return tab.tab.selected_item_index, tab.tab.tab_items[tab.tab.selected_item_index].tab_item.Value
 }
 
 func (tab *Tab[T]) GetTabByIndex(index int) (text string, value T) {
-	tab_item := &tab.tab.tab_items[index]
-	return tab_item.Text, tab_item.Value
+	tab_item := tab.tab.tab_items[index]
+	return tab_item.tab_item.Text, tab_item.tab_item.Value
 }
 
 func (tab *Tab[T]) SelectTabItemByIndex(index int) {
-	tab.tab.selected_index = index
-	tab.tab.on_select_fn(&tab.tab.tab_items[index], index)
+	tab.tab.selected_item_index = index
+	tab.tab.on_select_fn(tab.tab.tab_items[index].tab_item, index)
 }
 
 func (tab *Tab[T]) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
