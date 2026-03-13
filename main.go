bc2c0d2ada46b98d4e5302a38cf79db42db97e50
Udan Jayanith <j.udanjayanith@gmail.com>
2026-03-13 22:27:32 +0530

    Add app icon

--- main.go
+++ main.go
@@ -1,36 +1,49 @@
 package main
 
 import (
+	"bytes"
+	_ "embed"
 	"fmt"
 	"image"
+	_ "image/png"
+	"log"
 	"os"
 
 	gui "github.com/guigui-gui/guigui"
 	"github.com/hajimehoshi/ebiten/v2"
 
-	"API-Client/widgets/request/page"
+	request_page "API-Client/widgets/request/page"
 	Welcome "API-Client/widgets/welcome"
 )
 
 type Root struct {
 	gui.DefaultWidget
-	
+
 	welcome_page_widget Welcome.Welcome
 	request_page_widget request_page.RequestPage
 }
 
 func (r *Root) Build(context *gui.Context, adder *gui.ChildAdder) error {
-	adder.AddWidget(&r.request_page_widget)
+	adder.AddWidget(&r.welcome_page_widget)
 	return nil
 }
 
 func (r *Root) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBounds, layouter *gui.ChildLayouter) {
-	layouter.LayoutWidget(&r.request_page_widget, widgetBounds.Bounds())
+	layouter.LayoutWidget(&r.welcome_page_widget, widgetBounds.Bounds())
 }
 
+//go:embed icon.png
+var zbolt_icon_bytes []byte
+
 func main() {
+	zebolt_icon, _, err := image.Decode(bytes.NewReader(zbolt_icon_bytes))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	ebiten.SetWindowIcon([]image.Image{zebolt_icon})
+
 	op := &gui.RunOptions{
-		Title: "API Client",
+		Title:         "API Client",
 		WindowMinSize: image.Pt(800, 444),
 		RunGameOptions: &ebiten.RunGameOptions{
 			ApplePressAndHoldEnabled: true,
