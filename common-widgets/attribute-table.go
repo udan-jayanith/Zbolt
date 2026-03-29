b53896480a1bc64108ad18ecce592becd6341868
Udan Jayanith <j.udanjayanith@gmail.com>
2026-03-29 18:05:22 +0530

    changes

--- common-widgets/attribute-table.go
+++ common-widgets/attribute-table.go
@@ -48,27 +48,47 @@ func (w *table_row_widget) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBoun
 		Direction: gui.LayoutDirectionHorizontal,
 		Gap:       w.gap(ctx),
 		Padding:   w.padding(ctx),
-		Items: []gui.LinearLayoutItem{
-			{
-				Widget: &w.checkbox,
-			},
-			{
-				Widget: &w.key_column,
-				Size:   gui.FlexibleSize(1),
-			},
-			{
-				Widget: &w.vr,
-			},
-			{
-				Widget: &w.value_column,
-				Size:   gui.FlexibleSize(1),
-			},
-			{
-				Widget: w.row_delete_btn,
-			},
+		Items: []gui.LinearLayoutItem{},
+	}
+
+	left_column_layout := layout
+	left_column_layout.Padding = gui.Padding{}
+	left_column_layout.Items = []gui.LinearLayoutItem{
+		{
+			Widget: &w.checkbox,
+		},
+		{
+			Widget: &w.key_column,
+			Size:   gui.FlexibleSize(1),
 		},
 	}
 
+	right_column_layout := layout
+	right_column_layout.Padding = gui.Padding{}
+	right_column_layout.Items = []gui.LinearLayoutItem{
+		{
+			Widget: &w.value_column,
+			Size:   gui.FlexibleSize(1),
+		},
+		{
+			Widget: w.row_delete_btn,
+		},
+	}
+
+	layout.Items = []gui.LinearLayoutItem{
+		{
+			Layout: left_column_layout,
+			Size: gui.FlexibleSize(1),
+		},
+		{
+			Widget: &w.vr,
+		},
+		{
+			Layout: right_column_layout,
+			Size: gui.FlexibleSize(1),
+		},
+	}
+	
 	layout.LayoutWidgets(ctx, widgetBounds.Bounds(), layouter)
 }
 
@@ -97,11 +117,33 @@ func (w *table_row_widget) HandlePointingInput(ctx *gui.Context, widgetBounds *g
 
 type attribute_table struct {
 	gui.DefaultWidget
-	rows []table_row_widget
+	vr                       VerticalLine
+	key_header, value_header widget.Text
+	rows                     []table_row_widget
 }
 
 func (at *attribute_table) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
+	at.key_header.SetValue("Key")
+	at.key_header.SetBold(true)
+	at.key_header.SetVerticalAlign(widget.VerticalAlignMiddle)
+	adder.AddWidget(&at.key_header)
+
+	adder.AddWidget(&at.vr)
+
+	at.value_header.SetValue("Value")
+	at.value_header.SetBold(true)
+	at.value_header.SetVerticalAlign(widget.VerticalAlignMiddle)
+	adder.AddWidget(&at.key_header)
+
+	for i, _ := range at.rows {
+		adder.AddWidget(&at.rows[i])
+	}
+
 	return nil
+}
+
+func (at *attribute_table) header_height(ctx *gui.Context) int {
+	return widget.LineHeight(ctx) + (widget.UnitSize(ctx)/3)*2
 }
 
 func (at *attribute_table) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBounds, layouter *gui.ChildLayouter) {
@@ -109,6 +151,30 @@ func (at *attribute_table) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBoun
 		Direction: gui.LayoutDirectionVertical,
 		Items:     make([]gui.LinearLayoutItem, 0, len(at.rows)),
 	}
+
+	u := widget.UnitSize(ctx)
+	header_layout := gui.LinearLayout{
+		Direction: gui.LayoutDirectionHorizontal,
+		Gap:       u / 4,
+		Padding:   basic.NewPadding(u / 3),
+		Items: []gui.LinearLayoutItem{
+			{
+				Widget: &at.key_header,
+				Size:   gui.FlexibleSize(1),
+			},
+			{
+				Widget: &at.vr,
+			},
+			{
+				Widget: &at.value_header,
+				Size:   gui.FlexibleSize(1),
+			},
+		},
+	}
+	layout.Items = append(layout.Items, gui.LinearLayoutItem{
+		Layout: header_layout,
+		Size:   gui.FixedSize(at.header_height(ctx)),
+	})
 
 	for i, _ := range at.rows {
 		layout.Items = append(layout.Items, gui.LinearLayoutItem{
@@ -128,6 +194,7 @@ func (at *attribute_table) Measure(ctx *gui.Context, constraints gui.Constraints
 		point.X = widget.UnitSize(ctx) * 6
 	}
 
+	point.Y = at.header_height(ctx)
 	for i, _ := range at.rows {
 		point.Y += at.rows[i].Measure(ctx, gui.Constraints{}).Y
 	}
@@ -136,16 +203,14 @@ func (at *attribute_table) Measure(ctx *gui.Context, constraints gui.Constraints
 }
 
 func (at *attribute_table) Draw(ctx *gui.Context, widgetBounds *gui.WidgetBounds, dst *ebiten.Image) {
-	if len(at.rows) < 2 {
-		return
-	}
-	
 	b := widgetBounds.Bounds()
+	b.Min.Y += at.header_height(ctx)
+
 	line_color := draw_color.ScaleAlpha(draw_color.Color(ctx.ResolvedColorMode(), draw_color.ColorTypeBase, 0), 6/32.0)
 	width := 1 * float32(ctx.Scale())
-	
+
 	for i, _ := range at.rows {
-		b.Min.Y += at.rows[i].Measure(ctx, gui.Constraints{}).Y
 		vector.StrokeLine(dst, float32(b.Min.X), float32(b.Min.Y), float32(b.Max.X), float32(b.Min.Y), width, line_color, true)
+		b.Min.Y += at.rows[i].Measure(ctx, gui.Constraints{}).Y
 	}
 }
