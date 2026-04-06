7c13a2a8de7796a63c6d48a67fd202c13eaeca70
Udan Jayanith <j.udanjayanith@gmail.com>
2026-04-07 00:41:59 +0530

    Refactored request input bar

--- widgets/request/page/http/request-input-bar.go
+++ widgets/request/page/http/request-input-bar.go
@@ -3,6 +3,7 @@ package http_widget
 import (
 	"API-Client/icons"
 	"image"
+	"log"
 
 	gui "github.com/guigui-gui/guigui"
 	widget "github.com/guigui-gui/guigui/basicwidget"
@@ -16,28 +17,16 @@ type request_input_bar_widget struct {
 	method_list           []string
 	selected_method_index int
 
-	input_widget       widget.TextInput
-	request_btn_widget widget.Button
-	open_in_icon       *ebiten.Image
-	open_in            widget.Button
-	on_request         func(ctx *gui.Context, url, method string)
-}
+	url_input              widget.TextInput
+	url_input_not_editable bool
 
-func (rib *request_input_bar_widget) SelectMethod(method string) {
-	rib.init_methods()
-	for i, v := range rib.method_list {
-		if v != method {
-			continue
-		}
+	request_button_text          string
+	request_btn_widget           widget.Button
+	on_request_button_clicked_fn func(ctx *gui.Context, value string)
 
-		rib.selected_method_index = i
-		break
-	}
-}
-
-func (rib *request_input_bar_widget) Method() string {
-	rib.init_methods()
-	return rib.method_list[rib.selected_method_index]
+	open_in_icon *ebiten.Image
+	open_in      widget.Button
+	on_request   func(ctx *gui.Context, url, method string)
 }
 
 func (rib *request_input_bar_widget) init_methods() {
@@ -54,6 +43,57 @@ func (rib *request_input_bar_widget) init_methods() {
 	}
 }
 
+func (rib *request_input_bar_widget) select_method(method string) {
+	rib.init_methods()
+	for i, v := range rib.method_list {
+		if v != method {
+			continue
+		}
+
+		rib.selected_method_index = i
+		break
+	}
+}
+
+func (rib *request_input_bar_widget) method() string {
+	rib.init_methods()
+	return rib.method_list[rib.selected_method_index]
+}
+
+func (rib *request_input_bar_widget) url_input_value() string {
+	return rib.url_input.Value()
+}
+
+func (rib *request_input_bar_widget) set_url_input_value(value string) {
+	rib.url_input.SetValue(value)
+}
+
+func (rib *request_input_bar_widget) set_url_input_editable(editable bool) {
+	rib.url_input_not_editable = !editable
+}
+
+func (rib *request_input_bar_widget) on_url_input_value_changed(fn func(context *gui.Context, text string, committed bool)) {
+	rib.url_input.OnValueChanged(fn)
+}
+
+func (rib *request_input_bar_widget) init_request_button_text() {
+	if rib.request_button_text == "" {
+		// TODO: Set a icon
+		rib.request_button_text = "Request"
+	}
+}
+
+func (rib *request_input_bar_widget) on_request_button_clicked(fn func(ctx *gui.Context, value string)) {
+	rib.on_request_button_clicked_fn = fn
+}
+
+func (rib *request_input_bar_widget) set_request_button_value(value string) {
+	if value == "Request" || value == "Cancel" {
+		rib.request_button_text = value
+	}
+	log.Fatalln("Unknown request button text:", value)
+}
+
 func (rib *request_input_bar_widget) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
 	rib.method_select_widget.SetItemsByStrings(rib.method_list)
 	rib.method_select_widget.OnItemSelected(func(_ *gui.Context, index int) {
@@ -63,8 +103,8 @@ func (rib *request_input_bar_widget) Build(ctx *gui.Context, adder *gui.ChildAdd
 	adder.AddWidget(&rib.method_select_widget)
 
 	// Set input bar editable or not based on url pattern
-	rib.input_widget.SetEditable(true)
-	adder.AddWidget(&rib.input_widget)
+	rib.url_input.SetEditable(!rib.url_input_not_editable)
+	adder.AddWidget(&rib.url_input)
 
 	if rib.open_in_icon == nil {
 		rib.open_in_icon = icons.Store.Open("open-in")
@@ -72,9 +112,18 @@ func (rib *request_input_bar_widget) Build(ctx *gui.Context, adder *gui.ChildAdd
 	rib.open_in.SetIcon(rib.open_in_icon)
 	adder.AddWidget(&rib.open_in)
 
-	rib.request_btn_widget.SetText("Request")
-	rib.request_btn_widget.SetType(widget.ButtonTypePrimary)
+	rib.init_request_button_text()
+	if rib.request_button_text == "Request" {
+		rib.request_btn_widget.SetType(widget.ButtonTypePrimary)
+	} else {
+		rib.request_btn_widget.SetType(widget.ButtonTypeNormal)
+	}
+	rib.request_btn_widget.SetText(rib.request_button_text)
+	rib.request_btn_widget.OnDown(func(ctx *gui.Context) {
+		rib.on_request_button_clicked_fn(ctx, rib.request_button_text)
+	})
 	adder.AddWidget(&rib.request_btn_widget)
+
 	return nil
 }
 
@@ -93,7 +142,7 @@ func (rib *request_input_bar_widget) Layout(ctx *gui.Context, widgetBounds *gui.
 							Widget: &rib.method_select_widget,
 						},
 						{
-							Widget: &rib.input_widget,
+							Widget: &rib.url_input,
 							Size:   gui.FlexibleSize(1),
 						},
 						{
@@ -119,15 +168,4 @@ func (rib *request_input_bar_widget) Measure(ctx *gui.Context, constraints gui.C
 		return image.Pt(u*10, h)
 	}
 	return image.Pt(u*10, u*2)
-}
-
-func (rib *request_input_bar_widget) OnRequest(fn func(ctx *gui.Context, url, method string)) {
-	rib.request_btn_widget.OnDown(func(ctx *gui.Context) {
-		method, _ := rib.method_select_widget.SelectedItem()
-		fn(ctx, rib.input_widget.Value(), method.Text)
-	})
-}
-
-func (rib *request_input_bar_widget) OnOpenIn(fn func(ctx *gui.Context)) {
-	rib.open_in.OnDown(fn)
 }
