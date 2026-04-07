75849ede2d4aa5b61447af525139873a89d2ec6f
Udan Jayanith <j.udanjayanith@gmail.com>
2026-04-08 00:58:52 +0530

    changes

--- widgets/request/page/http/http-widget.go
+++ widgets/request/page/http/http-widget.go
@@ -3,7 +3,6 @@ package http_widget
 import (
 	CommonWidgets "API-Client/common-widgets"
 	messages "API-Client/massages"
-	attr "API-Client/widgets/request"
 	"API-Client/widgets/request/def"
 	"image"
 	"net/url"
@@ -12,7 +11,6 @@ import (
 	gui "github.com/guigui-gui/guigui"
 	widget "github.com/guigui-gui/guigui/basicwidget"
 	"github.com/hajimehoshi/ebiten/v2"
-	// "image"
 )
 
 type HTTP_Widget struct {
@@ -22,8 +20,7 @@ type HTTP_Widget struct {
 	vr              CommonWidgets.VerticalLine
 	response_widget response_widget
 
-	popup_widget *widget.Popup
-	popup_size   *image.Point
+	// TODO: add a popup and a url_panel_widget widget here.
 
 	req  *def.Request
 	data *def.HTTP_Data
@@ -34,11 +31,7 @@ func (brp *HTTP_Widget) RequestType() def.RequestType {
 	return def.HTTP
 }
 
-func (brp *HTTP_Widget) SetPopupWidget(w *widget.Popup, popup_size *image.Point) {
-	brp.popup_widget = w
-	brp.popup_size = popup_size
-}
-
+// SetReq runs when switching tabs and tab data are passed to this.
 func (brp *HTTP_Widget) SetReq(req *def.Request) {
 	if req.Type != def.HTTP {
 		panic("Invalid request type")
@@ -76,113 +69,29 @@ func (brp *HTTP_Widget) SetReq(req *def.Request) {
 	brp.response_widget.SetResponseBody(&temp.Body)
 }
 
-func (brp *HTTP_Widget) update() {
-	d := brp.data
-	brp.response_widget.OnAutowrapToggle(func(ctx *gui.Context, value bool) {
-		d.ResponseConfig.AutoWrap = value
-	})
-
-	brp.response_widget.OnFormatToggle(func(ctx *gui.Context, value bool) {
-		d.ResponseConfig.Formate = value
-	})
-
-	brp.response_widget.SetResponseData(brp.data.ResponseData())
-
-	brp.request_widget.OnURLInputChange(func(ctx *gui.Context, text string, committed bool, by_user bool) {
-		if !by_user {
-			return
-		}
-		// TODO: ask user to clear before cleaning the pattern
-
-		brp.data.URL.Path.Pattern.Pattern = ""
-		brp.data.URL.Path.Pattern.Attributes = []attr.Attribute{}
-		u, err := url.Parse(text)
-		if err != nil {
-			messages.Alerts.Push(err.Error())
-		}
-
-		brp.data.URL.Path.RawPath = u.Path
-		u.Path = ""
-		//TODO:handle raw query if committed
-		u.RawQuery = ""
-		brp.data.URL.BaseURL = u.String()
-	})
-
-	if time.Now().Sub(brp.t).Seconds() < 1 {
-		return
-	}
-	brp.t = time.Now()
-
-	//TODO: update url preview every second
-	brp.SyncData()
-}
-
-// TODO: run SyncData before switching tabs
+// TODO: SyncData should be run to save data before switching tabs, closing tabs or closing the app.
 func (brp *HTTP_Widget) SyncData() {
 	brp.data.Headers = brp.request_widget.Headers()
 	brp.data.Parameters = brp.request_widget.Parameters()
 	brp.data.SetSelectedRequestTab(brp.request_widget.SelectedTab())
 	brp.data.Method = brp.request_widget.Method()
-}
-
-func (brp *HTTP_Widget) handle_popup() {
-	if brp.popup_widget == nil {
-		return
-	}
-
-	brp.request_widget.input_bar_widget.OnOpenIn(func(ctx *gui.Context) {
-		url_panel := get_url_panel(ctx)
-		*brp.popup_size = url_panel.Measure(ctx, gui.Constraints{})
-
-		u, err := url.Parse(brp.data.URL.BaseURL)
-		if err != nil {
-			messages.Alerts.Push(err.Error())
-		}
-		if brp.data.URL.IsPattern() {
-			u.Path = brp.data.URL.Path.Pattern.Pattern
-			url_panel.content.query.SetRows(brp.data.URL.Path.Pattern.Attributes)
-		} else {
-			u.Path = brp.data.URL.Path.RawPath
-		}
-		url_panel.SetURL(u, ctx)
-
-		brp.popup_widget.SetContent(url_panel)
-		brp.popup_widget.SetOpen(true)
-	})
-
-	brp.popup_widget.OnClose(func(ctx *gui.Context, reason widget.PopupCloseReason) {
-		url_panel_content := get_url_panel(ctx).content
-
-		u, is_pattern := url_panel_content.generate_url()
-		brp.request_widget.SetURL_InputValue(u.String())
-		path := u.Path
-		u.Path = ""
-		brp.data.URL.BaseURL = u.String()
-		if is_pattern {
-			brp.data.URL.Path.RawPath = ""
-			brp.data.URL.Path.Pattern.Pattern = url_panel_content.path.Value()
-			brp.data.URL.Path.Pattern.Attributes = url_panel_content.query.Rows()
-		} else {
-			brp.data.URL.Path.Pattern.Pattern = ""
-			brp.data.URL.Path.Pattern.Attributes = []attr.Attribute{}
-			brp.data.URL.Path.RawPath = path
-		}
-
-		url_panel_content.query.SetRows([]attr.Attribute{})
-		brp.popup_widget.OnClose(func(_ *gui.Context, _ widget.PopupCloseReason) {})
-	})
+	// TODO: sync the request.
 }
 
 func (brp *HTTP_Widget) Build(ctx *gui.Context, adder *gui.ChildAdder) error {
 	ctx.SetPreferredColorMode(ebiten.ColorModeDark)
 
-	brp.update()
-	brp.handle_popup()
-
+	// TODO: handle the url panel popup here
+	 
 	adder.AddWidget(&brp.request_widget)
 	adder.AddWidget(&brp.vr)
 	adder.AddWidget(&brp.response_widget)
 	return nil
+}
+
+// TODO: finish this
+func (brp *HTTP_Widget) url_panel_popup_size() image.Rectangle {
+	return image.Rectangle{}
 }
 
 func (brp *HTTP_Widget) Layout(ctx *gui.Context, widgetBounds *gui.WidgetBounds, layouter *gui.ChildLayouter) {
